86adb23a43dc1109d98906beb595d0d93544a254
baotheo1999 <nguyenquanganhbao148@gmail.com>
2021-06-10 14:14:47 +0700

    update active category

--- my-app/src/components/CategoryContainer.js
+++ my-app/src/components/CategoryContainer.js
@@ -3,15 +3,26 @@ import React from 'react';
 import CategoryItems from './CategoryItems';
 
 class CategoryContainer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: ""
+        };
+    }
+
+    handleOnClick = (id) => {
+        this.setState({ active: id });
+    };
     
     render() {        
         const { data } = this.props;    
-        
+        console.log(this.state.active);
         return (
             <div className="main-container__left-navmenu">
                 <ul className="main-container__left-navmenu__list">
                     {data.map(item => item.ListProduct.length !== 0 ? (
-                        <CategoryItems categories={item} key={item._id} />
+                        <CategoryItems onClick={() => this.handleOnClick(item._id)} className={this.state.active === item._id ? 'active' : ''} categories={item} key={item._id} />
                     ) : null)}
                 </ul>
             </div>
--- my-app/src/components/CategoryItems.js
+++ my-app/src/components/CategoryItems.js
@@ -2,39 +2,10 @@ import React from 'react';
 
 
 class CategoryItems extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            active: ""
-        };
-        this.myRef = React.createRef();
-    }
-
-    
-
-    handleClick = (id) => {
-        this.setState({ active: id });
-        // console.log(e.target);
-        // if(e.currentTarget){
-        //     console.log('bb');
-        //     this.setState({ active: id });
-        // }else{
-        //     this.setState({ active: "" });
-        //    console.log('aa');
-        // }
-
-        document.getElementsByClassName()
-        
-      };
-
-
     render() {
-        console.log(this.myRef);
-        const { categories } = this.props;
-        // console.log(this.state.active);
+        const { categories, bao } = this.props;
         return (
-            <li ref={this.myRef} className={categories._id === this.state.active ? "active" : "main-container__left-navmenu__items"} onClick={() => this.handleClick(categories._id)}
-            ><a href={`#${categories._id}`}>{categories.name}</a></li>
+            <li className={`main-container__left-navmenu__items ${this.props.className}`}><a  onClick={this.props.onClick} href={`#${categories._id}`}>{categories.name}</a></li>
         );
     }
 }
